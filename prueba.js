6e7d017c44a8a79728ac8a9bc21e4116a55863ce
JuanManuelGasinghini <113570244+JuanManuelGasinghini@users.noreply.github.com>
2023-07-20 14:58:21 -0300

    agregado carrito y stock

--- prueba.js
+++
@@ -1,8 +0,0 @@
-let nombreIngresado = prompt("Ingrese su nombre");
-
-console.log("Mensaje de prueba");
-
-let nombre = "John Doe"
-console.log(nombre)
-
-alert("Hola!!");
---
+++ stock.js
@@ -0,0 +1,6 @@
+const productos = [
+    { id: 1, nombre: "Zapatillas", precio: 5000 },
+    { id: 2, nombre: "Camisa", precio: 1800 },
+    { id: 3, nombre: "Pantalón", precio: 2000 },
+    { id: 4, nombre: "Campera", precio: 7300 }
+  ];
